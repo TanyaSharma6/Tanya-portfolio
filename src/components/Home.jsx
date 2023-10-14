14016ed6cc6eab53ae65c5c47a3c8e9f199513c3
TanyaSharma6 <143124897+TanyaSharma6@users.noreply.github.com>
2023-10-14 12:53:15 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ResponsiveAppBar from "./ResponsiveAppBar";
 import Intro from "./Intro";
 import TechStack from "./TechStack";
-import Education from "./Education";
 import Contact from "./Contact";
 
 
